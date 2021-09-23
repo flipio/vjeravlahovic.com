d30a1b74c3d583fc0ac8c23e5c7e74aa957e0640
Filip Jelic <jelic.filip@gmail..com>
2021-09-23 17:25:15 +0200

    title + homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import './App.scss';
 import { BusinessCard } from './components/BusinessCard';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <title>Vjera Vlahović</title>
+      </Helmet>
+
       <BusinessCard />
     </div>
   );
